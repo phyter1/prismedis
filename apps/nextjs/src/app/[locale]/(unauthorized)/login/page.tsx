c20d37e81e30ff5d5bf84ce537165048aeb86df7
Ryan Lowe <ryan.phyter.1@gmail.com>
2024-02-28 22:14:08 -0500

    ming

--- apps/nextjs/src/app/[locale]/(unauthorized)/login/page.tsx
+++ apps/nextjs/src/app/[locale]/(unauthorized)/login/page.tsx
@@ -1,5 +1,13 @@
 import { providers } from "@prismedis/auth"
+import { getI18n } from "@prismedis/locales/server"
 import { ActionForm } from "@prismedis/ui/action-form"
+
+export async function generateMetadata() {
+  const t = await getI18n()
+  return {
+    title: t("auth.page_title"),
+  }
+}
 
 export default async function Page() {
   return (
