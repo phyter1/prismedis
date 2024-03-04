87b811031c2adc6e2bc6da0d48e3a37e0c777252
Ryan Lowe <ryan.phyter.1@gmail.com>
2024-03-04 15:16:58 -0500

    remove tasks

--- packages/db/src/mongodb/index.ts
+++ packages/db/src/mongodb/index.ts
@@ -1,12 +1,10 @@
 import { ObjectId } from "mongodb"
 
 import { connect } from "./papr"
-import Task from "./schema/task"
 import Verification from "./schema/verification"
 
 export const db = {
   ObjectId,
-  task: Task,
   verification: Verification,
 }
 
